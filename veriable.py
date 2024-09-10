546cef110df8767bea0c1c5c7c45d9a61a23fce8
Oleg196-Ek <zhookov@mail.ru>
2024-09-10 11:17:13 +0300

    Add files via upload

---
+++ veriable.py
@@ -0,0 +1,5 @@
+name = "Курс: Python,"
+new_name = '12,'
+hours = "1,5,"
+one_task = '0.125 часа'
+print(name,'всего задач:', new_name, 'затрачено часов', hours,'среднее время выполнения,', one_task)
